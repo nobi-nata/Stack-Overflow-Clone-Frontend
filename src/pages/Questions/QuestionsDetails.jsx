2216bc03ae3d7bc98fb3ce211a8ab122694e1723
Laxmikant <guravlaxmikant01@gmail.com>
2022-12-28 19:02:17 +0530

    frontend-url

--- src/pages/Questions/QuestionsDetails.jsx
+++ src/pages/Questions/QuestionsDetails.jsx
@@ -72,7 +72,7 @@ const QuestionsDetails = () => {
     const dispatch = useDispatch()
     const User = useSelector((state) => (state.currentUserReducer))
     const location = useLocation()
-    const url = 'http://localhost:3000'
+    const url = 'https://stackoverflow-1wdc.onrender.com'
 
     const handlePostAns = (e, answerLength) => {
         e.preventDefault()
